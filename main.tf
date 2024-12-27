cc81ab0712b35b5992b2e080c4f2df17408e5b6e
Felipe Fischmann <felipe.fischmann@gmail.com>
2024-12-27 08:06:11 -0300

    feat: add autoscaling

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-// data sources get information about deployed infrastructure
+# data sources get information about deployed infrastructure
 data "aws_ami" "app_ami" {
   most_recent = true
 
@@ -30,18 +30,41 @@ module "blog_vpc" {
   }
 }
 
-// "aws_instance" is the resource type. these type correspond to actual resources depending on the provider.
-// "blog" is the name terraform will use for this resource.
-// aws creates a default VPC for each account, and here we just use the default
-resource "aws_instance" "blog" {
-  ami                    = data.aws_ami.app_ami.id // ami = basic image to use
-  instance_type          = var.instance_type
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
+# "aws_instance" is the resource type. these type correspond to actual resources depending on the provider.
+# "blog" is the name terraform will use for this resource.
+# aws creates a default VPC for each account, and here we just use the default
+# resource "aws_instance" "blog" {
+#   ami                    = data.aws_ami.app_ami.id # ami = basic image to use
+#   instance_type          = var.instance_type
+#   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
-  subnet_id = module.blog_vpc.public_subnets[0] # get first subnet
+#   subnet_id = module.blog_vpc.public_subnets[0] # get first subnet
+
+#   tags = {
+#     Name = "HelloWorld"
+#   }
+# }
+
+# Replaces the instance above
+module "autoscaling" {
+  source   = "terraform-aws-modules/autoscaling/aws"
+  version  = "8.0.1"
+  name     = "blog"
+  min_size = 1
+  max_size = 2
+  # not configuring autoscaling per load
+
+  # network config
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  # target_group_arns = # no longer applies?
+  security_groups = [module.blog_sg.security_group_id]
+
+  # application config
+  image_id      = data.aws_ami.app_ami.id
+  instance_type = var.instance_type
 
   tags = {
-    Name = "HelloWorld"
+    Name = "HelloAutoscalingWorld"
   }
 }
 
@@ -94,7 +117,8 @@ module "blog_alb" {
       protocol    = "HTTP"
       port        = 80
       target_type = "instance"
-      target_id   = aws_instance.blog.id
+      # target_id   = aws_instance.blog.id
+      target_id   = module.autoscaling.autoscaling_group_id
     }
   }
 
@@ -104,16 +128,16 @@ module "blog_alb" {
   }
 }
 
-// a module is a group of resources
+# a module is a group of resources
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.2.0"
   name    = "blog"
 
-  # vpc_id              = data.aws_vpc.default.id // from the data block above # old
+  # vpc_id              = data.aws_vpc.default.id # from the data block above # old
   vpc_id              = module.blog_vpc.vpc_id
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
-  ingress_cidr_blocks = ["0.0.0.0/0"] // = open to everything
+  ingress_cidr_blocks = ["0.0.0.0/0"] # = open to everything
   egress_rules        = ["all-all"]
   egress_cidr_blocks  = ["0.0.0.0/0"]
 }
