2f9a9656985dcbbd9c3a5d8c8ee1a4427937d57a
Felipe Fischmann <felipe.fischmann@gmail.com>
2024-12-27 07:22:47 -0300

    feat: remove old VPC feat: add load balancer

--- main.tf
+++ main.tf
@@ -15,13 +15,6 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-#region VPC
-## original unused
-data "aws_vpc" "default" {
-  default = true // just use defaults
-}
-
-## new, module-based
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
@@ -36,7 +29,6 @@ module "blog_vpc" {
     Environment = "dev"
   }
 }
-#endregion
 
 // "aws_instance" is the resource type. these type correspond to actual resources depending on the provider.
 // "blog" is the name terraform will use for this resource.
@@ -53,6 +45,63 @@ resource "aws_instance" "blog" {
   }
 }
 
+module "blog_alb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name            = "blog-alb"
+  vpc_id          = module.blog_vpc.vpc_id
+  subnets         = module.blog_vpc.public_subnets
+  security_groups = module.blog_sg.security_group_id
+
+  # Security Group
+  #todo: figure out how to use the same rules as in module.blog_sg, maybe refactor them out?
+  security_group_ingress_rules = {
+    all_http = {
+      from_port   = 80
+      to_port     = 80
+      ip_protocol = "tcp"
+      description = "HTTP web traffic"
+      cidr_ipv4   = "0.0.0.0/0"
+    }
+    all_https = {
+      from_port   = 443
+      to_port     = 443
+      ip_protocol = "tcp"
+      description = "HTTPS web traffic"
+      cidr_ipv4   = "0.0.0.0/0"
+    }
+  }
+  security_group_egress_rules = {
+    all = {
+      ip_protocol = "-1"
+      cidr_ipv4   = "10.0.0.0/16"
+    }
+  }
+
+  listeners = {
+    ex-http = {
+      port     = 80
+      protocol = "HTTP"
+      target_groups = ["ex-instance"]
+    }
+  }
+
+  target_groups = { # where to send the traffic to
+    ex-instance = {
+      name_prefix = "blog-" # just to make it easier to identify
+      protocol    = "HTTP"
+      port        = 80
+      target_type = "instance"
+      target_id   = aws_instance.blog.id
+    }
+  }
+
+  tags = {
+    Environment = "Development"
+    Project     = "Example"
+  }
+}
+
 // a module is a group of resources
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
@@ -64,6 +113,6 @@ module "blog_sg" {
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"] // = open to everything
   egress_rules        = ["all-all"]
-  egress_cidr_blocks  = ["0.0.0.0/0"] // = open to everything
+  egress_cidr_blocks  = ["0.0.0.0/0"]
 }
 
